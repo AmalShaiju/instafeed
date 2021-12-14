f6b3f372e00daab9be67ea6721212f1388a7bbb2
Ashaiju1 <amalshaiju16@gmail.com>
2021-12-14 04:54:52 -0500

    Added google places sdk for location

--- app/src/main/java/nc/prog1415/activities/donor_view/activities/PrCreateActivity.java
+++ app/src/main/java/nc/prog1415/activities/donor_view/activities/PrCreateActivity.java
@@ -1,17 +1,67 @@
 package nc.prog1415.activities.donor_view.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
+import com.google.android.libraries.places.widget.Autocomplete;
+import com.google.android.libraries.places.widget.AutocompleteActivity;
+import com.google.android.libraries.places.widget.model.AutocompleteActivityMode;
+
+import java.util.Arrays;
+import java.util.List;
 
 import nc.prog1415.R;
 
 public class PrCreateActivity extends AppCompatActivity {
+    EditText txtLocation;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().setTitle("New Pickup Request");
         setContentView(R.layout.activity_pr_create);
+
+        txtLocation = findViewById(R.id.pr_create_txtLocation);
+
+        //Inititalize places
+        Places.initialize(getApplicationContext(),"AIzaSyBPWqoOuPUoqu0EZjszT1I_0g23D9MZrnw");
+
+        txtLocation.setFocusable(false);
+        txtLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Initializer place field list
+                List<Place.Field> fieldList = Arrays.asList(Place.Field.ADDRESS, Place.Field.LAT_LNG,Place.Field.NAME);
+
+                // Create intent
+                Intent intent = new Autocomplete.IntentBuilder(AutocompleteActivityMode.OVERLAY,fieldList).build(PrCreateActivity.this);
+
+                //start activity result
+                startActivityForResult(intent,100);
+
+            }
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == 100 && resultCode == RESULT_OK){
+            Place place = Autocomplete.getPlaceFromIntent(data);
+            txtLocation.setText(place.getAddress());
+        }else if(resultCode == AutocompleteActivity.RESULT_ERROR){
+            Status status = Autocomplete.getStatusFromIntent(data);
+
+            Toast.makeText(getApplicationContext(),status.getStatusMessage(), Toast.LENGTH_SHORT).show();
+        }
     }
 }
