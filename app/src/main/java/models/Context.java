c385fadf1ec792c2631bc16cdadd86c7b222c349
Ashaiju1 <amalshaiju16@gmail.com>
2021-12-16 04:02:07 -0500

    Updated Context

--- app/src/main/java/models/Context.java
+++ app/src/main/java/models/Context.java
@@ -8,11 +8,11 @@ import java.util.Optional;
 import java.util.stream.Collectors;
 
 public class Context {
-    public  static User loggedInUser;
+    public static User loggedInUser;
 
-    public  static ArrayList<User> userList = new ArrayList<User>();
+    public static ArrayList<User> userList;
 
-    public  static ArrayList<PickupRequest> prList = new ArrayList<PickupRequest>();
+    public static ArrayList<PickupRequest> prList;
 
     public static ArrayList<PickupRequest> getClaimedByMe() {
         return prList.stream().filter(p -> p.isClaimed() && p.getClaimedBy().equals(loggedInUser)).collect(Collectors.toCollection(ArrayList::new));
@@ -32,7 +32,7 @@ public class Context {
 
     public static boolean Login(String email, String password){
         User userToLogin;
-        userToLogin = userList.stream().filter(p -> p.getEmail().equals(email) && p.getPassword() == password).findFirst().orElse(null);
+        userToLogin = userList.stream().filter(p -> p.getEmail().toLowerCase().equals(email.toLowerCase()) && p.getPassword().equals(password)).findFirst().orElse(null);
         if(userToLogin != null){
             loggedInUser = userToLogin;
             return true;
@@ -40,17 +40,35 @@ public class Context {
         return false;
     }
 
+    public static boolean RegisterUser(User userToRegister){
+         boolean userExists = userList.contains(userToRegister);
+         if(!userExists){
+             userList.add(userToRegister);
+             return true;
+         }
+         return false;
+    }
+
+
     public static void Logout(){
         loggedInUser = null;
     }
 
 
     public static void seedData() throws Exception {
-        User claimer = new User("Saf","Shaiju","amalshaiju16@gmail.com","2898230814","1222 Natanial Crescent", "Wonderboy", UserType.CLAIMER);
-        User donor = new User("Amal","Shaiju","amalshaiju16@gmail.com","2898230814","1222 Natanial Crescent", "Wonderboy", UserType.DONOR);
-        PickupRequest p3 = new PickupRequest(donor,"test Address","Test item", LocalDateTime.now(),new ArrayList<byte[]>());
-        PickupRequest p1 = new PickupRequest(donor,"1222 Natanial Crescent","Table", LocalDateTime.now(),new ArrayList<byte[]>());
-        PickupRequest p2 = new PickupRequest(donor,"516 First Ave","Chair", LocalDateTime.now(),new ArrayList<byte[]>());
+        if(userList == null){
+            userList = new ArrayList<User>();
+        }
+
+        if(prList == null){
+            prList = new ArrayList<PickupRequest>();
+        }
+
+        User claimer = new User("Claimer","Adrin","Claimer@gmail.com","2898230814","1222 Natanial Crescent", "Wonderboy", "a", UserType.CLAIMER);
+        User donor = new User("Donor","Roshan","Donor@gmail.com","2898230814","1222 Natanial Crescent", "Wonderboy","a", UserType.DONOR);
+        PickupRequest p3 = new PickupRequest("test Address","Test item",null);
+        PickupRequest p1 = new PickupRequest("1222 Natanial Crescent","Table",null);
+        PickupRequest p2 = new PickupRequest("516 First Ave","Chair",null);
 
         p2.claimRequest(claimer);
         p3.claimRequest(claimer);
@@ -60,6 +78,7 @@ public class Context {
         prList.add(p2);
         prList.add(p3);
 
-        loggedInUser = claimer;
+        userList.add(claimer);
+        userList.add(donor);
     }
 }
